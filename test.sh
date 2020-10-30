9cba2938f553acf63fa994b12c4eaf969f445cbb
Ahmed Shalaby <kingshalaby@yahoo.com>
2020-10-30 15:54:08 +0200

    add test logic

--- test.sh
+++ test.sh
@@ -5,6 +5,18 @@ file_name=$1
 g++ "${file_name}".cpp -o "temp/${file_name}"
 echo -e 'successfully compiled \n evaluating...'
 
-## testing logic goes here
+ cat "test_cases/${file_name}" | while read line; do
+  case=($line)
+  expected=${case[*]: -1}
+  args=${case[*]::$((${#case[*]} - 1))}
+  evaluated=$(echo "${args}" | "temp/${file_name}")
+
+  if [ "$evaluated" != "$expected" ]
+  then
+      printf "wrong answer \n args passed: %s \n expected: %s \n got: %s", "$args", "$expected", "$evaluated"
+      exit 1
+  fi
+done
+echo "you answer is correct"
 
 exit 0
